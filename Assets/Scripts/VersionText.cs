83112810f62dfe8701457f28adc104632df7b2a0
sandorbence <szaniszlo47@gmail.com>
2025-12-07 20:33:42 +0100

    Add version text

---
+++ Assets/Scripts/VersionText.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class VersionText : MonoBehaviour
+{
+    private TextMeshProUGUI display;
+
+    private void Start()
+    {
+        this.display = GetComponent<TextMeshProUGUI>();
+        this.display.text = $"v{Application.version}";
+    }
+}
